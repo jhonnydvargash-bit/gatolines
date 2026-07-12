c6e420c143631e05faccd65c81373121e211f164
jhonny <jhonny@gatin.miau>
2026-07-12 19:42:21 -0400

    sdvsadvdsa

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return jsonify({
-        "status": "online",
-        "message": "¡Gatolines está corriendo perfectamente en Render!"
-    })
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
+    # host='0.0.0.0' permite que accedas desde tu móvil si está en el mismo WiFi
     app.run(debug=True)
+
